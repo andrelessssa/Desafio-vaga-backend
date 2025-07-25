39d1d693a428b133f8be695de82d8c279d0341a9
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-07-25 10:43:44 -0300

    :construction: concluido o CRUD de Imóveis
    :construction: concluido o CRUD de Imóveis

--- desafio/src/main/java/com/example/desafio/Dto/ImovelDTO.java
+++ desafio/src/main/java/com/example/desafio/Dto/ImovelDTO.java
@@ -2,6 +2,6 @@ package com.example.desafio.Dto;
 
 import com.example.desafio.model.enums.TipoImovel;
 
-public record ImovelDTO(TipoImovel tipoImovel, String bairro, String nome) {
+public record ImovelDTO(TipoImovel tipo, String bairro, String descricao) {
     
 }
